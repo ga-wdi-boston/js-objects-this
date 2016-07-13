b27b6e60a25c939f5a16560e864cb0c44be223e6
Antony Donovan <antony.donovan@generalassemb.ly>
2016-07-13 16:09:14 -0400

    Add tests for run methods.
    Fill out spec and add export of runs object.
    
    Also, clean up linter errors

--- spec/runs.spec.js
+++ spec/runs.spec.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+
+const runs = require('../lib/runs');
+
+describe('runs', function () {
+
+  describe('total distance', function () {
+
+    it('has the correct length', function () {
+      expect(runs.totalDistance()).to.equal(2600);
+    });
+
+  });
+
+  describe('longest run', function () {
+
+    it('has the correct length', function () {
+      expect(runs.longestRun()).to.equal(1400);
+    });
+
+  });
+
+  describe('average speed', function () {
+
+    it('has the correct value', function () {
+      expect(runs.averageSpeed().toLocaleString('en-US')).to.equal('1.857');
+    });
+
+  });
+
+});
